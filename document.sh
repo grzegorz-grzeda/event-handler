4976ca72441d4790fdd9d5f4dde1c95ef9cffa91
Grzegorz Grzęda <grzegorz.grzeda@g2labs.pl>
2023-10-12 13:04:43 +0200

    Updated license header

--- document.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
-$SCRIPT_DIR/packages/simple-documentation-library/document.py event-handler 1.1.1 "Simple event handler library" source documentation
